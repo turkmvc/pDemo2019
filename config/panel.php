f65b820c15ba4ac64bab53b5a7a5a17d62e5917d
Laravel Daily <info@laraveldaily.com>
2019-10-26 11:51:12 +0000

    QuickAdminPanel automatic commit

---
+++ config/panel.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'date_format'         => 'd.m.Y',
+    'time_format'         => 'H:i:s',
+    'primary_language'    => 'tr',
+    'available_languages' => [
+        'tr' => 'Turkish',
+        'en' => 'English',
+    ],
+];
---
+++ resources/views/admin/productTags/show.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.admin')
+@section('content')
+
+<div class="card">
+    <div class="card-header">
+        {{ trans('global.show') }} {{ trans('cruds.productTag.title') }}
+    </div>
+
+    <div class="card-body">
+        <div class="mb-2">
+            <table class="table table-bordered table-striped">
+                <tbody>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.productTag.fields.id') }}
+                        </th>
+                        <td>
+                            {{ $productTag->id }}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>
+                            {{ trans('cruds.productTag.fields.name') }}
+                        </th>
+                        <td>
+                            {{ $productTag->name }}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <a style="margin-top:20px;" class="btn btn-default" href="{{ url()->previous() }}">
+                {{ trans('global.back_to_list') }}
+            </a>
+        </div>
+
+        <nav class="mb-3">
+            <div class="nav nav-tabs">
+
+            </div>
+        </nav>
+        <div class="tab-content">
+
+        </div>
+    </div>
+</div>
+@endsection
